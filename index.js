ab78c7f0b5bd5f79d9e399544730c622700e386a
ianwalya <155696225+hotbite@users.noreply.github.com>
2025-08-23 03:16:51 +0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,12 @@
+var clse = document.getElementById('abt');
+var opn = document.getElementById('dwn');
+var card = document.getElementById('cd');
+
+function dwn(){
+    card.style.display = 'block';
+    
+}
+function abt(){
+    card.style.display = 'none';
+    
+}
